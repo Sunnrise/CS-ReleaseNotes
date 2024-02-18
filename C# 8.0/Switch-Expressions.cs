f5b834623fb685eafb791b843b476941b5f30f8c
alperen güneş <Alperen GNE>
2024-02-18 23:56:25 +0300

    Switch Expression with C# 8.0

---
+++ C# 8.0/Switch-Expressions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region General way
+
+        string message= "";
+        switch (DateTime.Now.DayOfWeek)     
+        {
+            case DayOfWeek.Sunday:
+                message="Today is Sunday";
+                break;
+            case DayOfWeek.Monday:
+                message = "Today is Monday";
+                break;
+            case DayOfWeek.Tuesday:
+                message = "Today is Tuesday";
+                break;
+            case DayOfWeek.Wednesday:
+                message = "Today is Wednesday";
+                break;
+            case DayOfWeek.Thursday:
+                message = "Today is Thursday";
+                break;
+            case DayOfWeek.Friday:
+                message = "Today is Friday";
+                break;
+            case DayOfWeek.Saturday:
+                message = "Today is Saturday";
+                break;
+            default:
+                break;
+            
+        }
+        Console.WriteLine(message);
+        #endregion
+
+        #region Switch Expression
+
+        string message = DateTime.Now.DayOfWeek switch
+        {
+            DayOfWeek.Sunday => "Today is Sunday",
+            DayOfWeek.Monday => "Today is Monday",
+            DayOfWeek.Tuesday => "Today is Tuesday",
+            DayOfWeek.Wednesday => "Today is Wednesday",
+            DayOfWeek.Thursday => "Today is Thursday",
+            DayOfWeek.Friday => "Today is Friday",
+            DayOfWeek.Saturday => "Today is Saturday"
+            
+        };
+        Console.WriteLine(message);
+
+
+
+
+        #endregion
+
+
+
+
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
