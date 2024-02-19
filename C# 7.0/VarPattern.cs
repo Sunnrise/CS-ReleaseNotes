409880a3e145c58bb54b50a7eb2d21827a261789
alperen güneş <Alperen GNE>
2024-02-19 23:40:19 +0300

    Var pattern and var keyword difference

---
+++ C# 7.0/VarPattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypePattern
+{
+    class TypePattern
+    {
+        static void Main(string[] args)
+        {
+
+            #region var pattern and var type difference
+            object z ="imSunnrise";
+            var a = "sun";//var keyword turns variable when compiling the code
+            if (z is var b)//var pattern turns variable when the code is running
+            {
+                Console.WriteLine($"z is a {b.GetType().Name} and its value is {b}");
+            }
+            
+            #endregion
+            
+
+            
+            Console.ReadKey();
+        }
+    }
+}
